1135671084b210db46a898281e24a3af529b74b1
Andrea E <ane28@hi.is>
2026-04-11 06:10:44 +0000

    jar packaged - assembly plugin

---
+++ src/main/java/is/vidmot/Main.java
@@ -0,0 +1,15 @@
+package is.vidmot;
+
+/******************************************************************************
+ *  Nafn    : Andrea Eiríksdóttir
+ *  T-póstur: ane28@hi.is
+ *
+ *  Lýsing  : Lýsing
+ *
+ *
+ *****************************************************************************/
+public class Main {
+    public static void main(String[] args) {
+        LudoApplication.main(args);
+    }
+}
